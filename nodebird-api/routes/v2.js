ea09b547e9079358cba7c146416e2172051fc9f2
som-seo <somlux0428@gmail.com>
2023-03-29 17:59:23 +0900

    웹 API 서버 만들기 230329_2

---
+++ nodebird-api/routes/v2.js
@@ -0,0 +1,26 @@
+const express = require('express');
+// const cors=require('cors');
+const { verifyToken, apiLimiter, corsWhenDomainMatches } = require('../middlewares');
+const { createToken, tokenTest, getMyPosts, getPostsByHashtag } = require('../controllers/v2');
+
+const router = express.Router();
+/* router.use(cors({
+     credentials: true,
+ }));
+*/
+
+router.use(corsWhenDomainMatches);
+
+// POST /v2/token
+router.post('/token', apiLimiter, createToken);
+
+// POST /v2/test
+router.get('/test', apiLimiter, verifyToken, tokenTest);
+
+// GET /v2/posts/my
+router.get('/posts/my', apiLimiter, verifyToken, getMyPosts);
+
+// GET /v2/posts/hashtag/:title
+router.get('/posts/hashtag/:title', apiLimiter, verifyToken, getPostsByHashtag);
+
+module.exports = router;
